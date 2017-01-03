70613b907af336301c38ec957196fc8e25ab0a25
Lee Tsai <lee.x.tsai@gmail.com>
2017-01-03 10:20:06 -0800

    [Node] Ch1-3 Node module.exports

---
+++ instantHello.js
@@ -0,0 +1,3 @@
+var name = "Lee";
+
+console.log("Hello, " + name + "!");
---
+++ talk/question.js
@@ -0,0 +1,6 @@
+var answer = "That is a great question!";
+
+module.exports.ask = function(question) {
+  console.log(question);
+  return answer;
+};
