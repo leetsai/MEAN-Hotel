51e9e8b8e09bca59ea4494679802060fca09c845
Lee Tsai <lee.x.tsai@gmail.com>
2017-01-04 14:43:08 -0800

    [Express] Middleware intro

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var path = require("path"); // a native node module useful for sendFile
 
 app.set('port', 1337); // create a variable called port and give it a value of 1337
 
+// the following is a middleware that console logs all the static files that get
+// loaded
+app.use(function(req, res, next) {
+  console.log(req.method, req.url);
+  next();
+});
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('/json', function(req, res) {
